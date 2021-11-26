487c79b6b0ed966811c8dc6ec51d3e91692e7afd
Maryna Kryvoruchenko <kry20001@byui.edu>
2021-11-26 11:26:34 -0700

    week11

--- lesson11/js/soda-springs-upcoming.js
+++ lesson11/js/soda-springs-upcoming.js
@@ -10,7 +10,7 @@ fetch(requestURL)
     const towns = jsonObject['towns'];
     
     for (let i = 0; i < towns.length; i++ ) {
-        if (towns[i].name == "Preston") {
+        if (towns[i].name == "Soda Springs") {
             let card = document.createElement('section');
             let p1 = document.createElement('p');
             p1.textContent = towns[i].events[0];
